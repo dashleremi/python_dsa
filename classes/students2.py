874c1bb5f21073c72ddab3d75d95a2cf48e91359
dashleremi <dashleremi@gmail.com>
2024-12-14 19:03:10 -1000

    more python

--- classes/students2.py
+++ classes/students2.py
@@ -1,5 +1,3 @@
-# first_name, last_name, courses
-
 # class names = camel case, capitalize
 class Student:
     def __init__(self, first, last, courses=None):
@@ -23,6 +21,42 @@ class Student:
         else:
             print(f"{course} not found")
 
+    def find_in_file(self, filename):
+        with open(filename) as f:
+            for line in f:
+                first_name, last_name, course_details = Student.prep_record(line.strip())
+                student_read_in = Student(first_name, last_name, course_details)
+                if self == student_read_in:
+                    return True
+            return False
+
+    def add_to_file(self, filename):
+        if self.find_in_file(filename):
+            return "Record already exits."
+        else:
+            record_to_add = Student.prep_to_write(self.first_name, self.last_name, self.courses)
+            with open(filename, "a+") as to_write:
+                to_write.write(record_to_add+"\n")
+
+            return "Record added."
+
+    @staticmethod
+    def prep_record(line):
+        line = line.split(":")
+        first_name, last_name = line[0].split(",")
+        course_details = line[1].rstrip().split(",")
+        return first_name, last_name, course_details
+    
+    @staticmethod
+    def prep_to_write(first_name, last_name, courses):
+        full_name = first_name + " " + last_name
+        courses = ",".join(courses)
+        return full_name + ":" + courses
+    
+    def __eq__(self, other):
+        return self.first_name == other.first_name \
+        and self.last_name == other.last_name
+
     def __len__(self):
         return len(self.courses)
     
@@ -34,7 +68,7 @@ class Student:
         return f"First name: {self.first_name.capitalize()}\nLast name: {self.last_name.capitalize()}\nCourses: {', '.join(map(str.capitalize, self.courses))}"
 
 # objects, instances of class Student
-courses_1 = ['python', 'rails', 'javascript']
+'''courses_1 = ['python', 'rails', 'javascript']
 courses_2 = ['java', 'rails', 'c']
 
 emi = Student("emi", "dashler", courses_1)
@@ -44,10 +78,32 @@ print(emi)
 print(repr(emi))
 print(john)
 print(repr(john))
-
+'''
 # prints out all classes that can be used with emi
 # includes the ones i custom made
 '''
 print(dir(emi))
 print(emi.__dir__)
 '''
+'''filename = "data.txt"
+emi = Student("emi","dashler",["python","ruby","javascript"])
+print(emi.find_in_file(filename))
+print(emi.add_to_file(filename))
+joe = Student("joey","schmoe",["python","ruby","javascript"])
+print(joe.find_in_file(filename))
+print(joe.add_to_file(filename))'''
+
+# inheritance, inherits all characteristics from Student
+class StudentAthlete(Student):
+    
+    # define own class explicitly under StudentAthlete that shouldn't be in Student
+    def __init__(self, first, last, courses=None, sport=None):
+        super().__init__(first, last, courses)
+        self.sport = sport
+
+courses = ["python", "ruby", "javascript"]
+jane = StudentAthlete("jane","doe",courses,"hockey")
+print(jane.sport)
+# print(help(jane))
+
+print(isinstance(jane, StudentAthlete))
--- files/files.py
+++ files/files.py
@@ -1,11 +1,29 @@
 # reading from and writing to files
 
 file_name = 'data.txt'
+
+# function to convert string to parameters for __init__
+def prep_record(line):
+    line = line.split(":")
+    first_name, last_name = line[0].split(",")
+    course_details = line[1].rstrip().split(",")
+    return first_name, last_name, course_details
+
+# function to convert Student object to string for write to file
+def prep_to_write(first_name, last_name, courses):
+    full_name = first_name + " " + last_name
+    courses = ",".join(courses)
+    return full_name + ":" + courses
+
 with open(file_name) as f:
     for line in f:
         print(line.strip())
+        first_name, last_name, course_details = prep_record(line)
+        print(first_name, last_name, course_details)
 
-record_to_add = "joe, schmo:python,ruby,javascript"
+to_write = prep_to_write(first_name, last_name, course_details)
+print(to_write)
+record_to_add = "joe,schmo:python,ruby,javascript"
 
-with open(file_name, "a+") as to_write:
-    to_write.write(record_to_add+"\n")
+# with open(file_name, "a+") as to_write:
+    # to_write.write(record_to_add+"\n")
